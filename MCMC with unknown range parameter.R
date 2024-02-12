4f026237488cccc846f3ad4ccb062213ab6bd5bd
Arghya Mukherjee <arghyam21@iitk.ac.in>
2024-02-12 15:53:57 +0530

    finish
    Arghya

---
+++ MCMC with unknown range parameter.R
@@ -0,0 +1,82 @@
+rm(list = ls())
+
+set.seed(3019)
+
+#Task 1 : Phi = 0.05
+
+library(fields)
+library(plot3D)
+library(geoR)
+
+N <- 5e2 # No. of spatial locations
+
+loc <- cbind(runif(N), runif(N)) #simulating 100 locations
+plot(loc)
+true_phi <- 0.05 #rep(0.05,100)  #true value of phi
+
+
+d <- rdist(loc) # Calculates the Euclidean distance matrix
+cov <- exp(-d/true_phi) # Calculates the correlation matrix
+Y <- c(t(chol(cov)) %*% rnorm(N)) # Draws a sample from multivariate normal
+
+scatter2D(x = loc[ , 1], y = loc[ , 2], colvar = Y, pch = 15)
+
+
+# MCMC Set up
+
+loglike_phi <- function(phi, Y){
+    
+    #nu <- 0.5
+    cov_mat <- exp(-d/phi)
+    chol.cov <- chol(cov_mat)
+    prec <- chol2inv(chol.cov)
+    like.out <-  -sum(log(diag(chol.cov))) - 0.5*sum((prec %*% Y) * Y)
+    return(like.out)
+
+}
+
+# Target
+
+target_phi <- function(phi, Y){
+  
+  return(loglike_phi(phi, Y) + dunif(phi, 0, 0.2, log = T))
+}
+
+# Metropolis-Hastings Algorithm
+
+mh_phi <- function(phi.init, iters, burnin){
+  
+  phi.chain <- array(dim = c(iters + burnin, 1))
+  phi.chain[1] <- phi.init 
+  
+  for (i in 2:(iters + burnin))
+  {
+    
+    proposal <- rnorm(n = 1, mean = phi.chain[i-1], sd = 0.05) 
+    
+    r = exp(target_phi(proposal, Y) - target_phi(phi.chain[i-1], Y))
+    
+    
+    acc_ratio = min(r,1)
+    
+    
+    if(runif(1) < acc_ratio){
+      
+      phi.chain[i] = proposal
+      
+    } else {
+      
+      phi.chain[i] = phi.chain[i-1]
+      
+    }
+  }
+  
+  acceptance = 1 - mean(duplicated(phi.chain[-(1:burnin)]))
+  
+  return(phi.chain)
+  
+}
+
+MCMC.out <- mh_phi(phi.init = 0.2, iters <- 1e2, burnin <- 1e2)
+
+plot(MCMC.out$phi.chain, xlab = "Iteration", ylab = "phi", type = "l")
