66f025440892c082863e289917b79ca0455a1b2e
Arghya Mukherjee <123416481+ArghyaStat@users.noreply.github.com>
2024-02-29 21:59:51 +0530

    Add files via upload

---
+++ TestingRcpp/MCMC_with_phi.R
@@ -0,0 +1,52 @@
+rm(list = ls())
+
+my_dir <- "C:/Users/HP/OneDrive - IIT Kanpur/arghya/IIT Kanpur PhD documents/Spatial and MCMC Research/Project1/Computing/Rcpp codes"
+
+setwd(my_dir)
+
+
+# Data Simulation
+
+# Simulating with  Phi = 0.1
+
+library(devtools)
+library(Rcpp)
+library(fields)
+library(plot3D)
+library(geoR)
+
+N <- 2e2 # No. of spatial locations 
+
+locations <- matrix(0, nrow = N, ncol = 2)
+
+locations[, 1] <- runif(N)
+locations[, 2] <- runif(N) #simulating 100 locations
+
+
+plot(locations)
+true_phi <- 0.1   #true value of phi
+
+
+d <- rdist(locations) # Calculates the Euclidean distance matrix
+cov <- exp(-d/true_phi) # Calculates the correlation matrix
+Y <- c(t(chol(cov)) %*% rnorm(N)) # Draws a sample from multivariate normal
+
+
+# Step 1: Save the C++ code to a file named source.cpp
+
+# Step 2: Compile the C++ code
+
+
+sourceCpp("mcmc_with_phi.cpp")
+
+# Set the parameters
+init_phi <- 0.5
+nu <- 0.5
+niters <- 1000
+h <- 0.05
+
+
+
+
+# Run the Metropolis-Hastings algorithm
+phi_chain <- metropolis_hastings(init_phi = 0.5, nu = 1, niters = 1000, h = 0.05, Y = Y, locations = locations)
