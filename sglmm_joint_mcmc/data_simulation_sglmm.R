ebcebc577f7a35a90193d9461f86429288b3036a
Arghya Mukherjee <123416481+ArghyaStat@users.noreply.github.com>
2024-06-24 13:34:37 +0530

    Update data_simulation_sglmm.R
    Updated with compatible to predictive analysis

--- sglmm_joint_mcmc/data_simulation_sglmm.R
+++ sglmm_joint_mcmc/data_simulation_sglmm.R
@@ -19,38 +19,43 @@ set.seed(1e3)
 # dimension of the random field
 q <- 2
 
-
 N <- 2e2 # No. of spatial locations
 
 #simulating N locations over [0,1]^2 in locations matrix
+
+N.pred <- ceiling(0.2*N)
+N.obs <- N - N.pred
 
 locations <- matrix(0, nrow = N, ncol = 2)
 
 locations[, 1] <- runif(N)
 locations[, 2] <- runif(N)
 
+pred.indices <- sample(1:N, N.pred)
+pred.loc <- locations[pred.indices, ]
+obs.loc <- locations[-pred.indices, ]
+
+joint.loc <- rbind(pred.loc, obs.loc)
+
 # Adding a mean term
 
-X = cbind(1, locations)
+X.obs <- cbind(1, obs.loc)
+X.pred <- cbind(1, pred.loc)
 
-#design matrix in concatenated form of dim (Np * 1)
-X.vec <- vec(t(X))
+X <- cbind(1, locations)
 
 #Number of features in the spatial model
-p <- ncol(X)
+p <- ncol(X.obs)
 
 # True value of regression matrix beta
-true.beta <- matrix(rnorm(p * q, mean = 0, sd = 1), nrow = p, ncol = q)
-
-#beta.mat is the kronecker product of the coefficient matrix across locations
-
-beta.mat <- diag(N) %x% t(true.beta)
-
-# Fixed effect (I_n * B^T) X
-mu.vec <- c(beta.mat %*% X.vec)
+true.beta <- matrix(rnorm(p * q, mean = 0, sd = 2), nrow = p, ncol = q)
 
 
-true.Sigma <- matrix(c(0.8, 0.4, 0.4, 0.5), nrow = q, ncol = q, byrow = TRUE)
+# Fixed effect 
+mu.vec <- vec(X %*% true.beta)
+
+
+true.Sigma <- matrix(c(3, 2, 2, 4), nrow = q, ncol = q, byrow = TRUE)
 
 # true.phi
 
@@ -62,7 +67,7 @@ true.nu <- 0.5
 
 #true.r
 
-true.r <- 0
+true.r <- 0.8
 
 # Calculates the Euclidean distance matrix
 
@@ -78,8 +83,8 @@ Omega <- true.Sigma %x% (true.r*K  + (1-true.r)*diag(N))
 
 # Generating the response vector of dimension Nq*1
 
-W.vec <- mu.vec + c(t(chol(Omega)) %*% rnorm(N*q))
-W.mat <- array(W.vec, dim = c(N, q))
+true.W.tilde.vec <- mu.vec + c(t(chol(Omega)) %*% rnorm(N*q))
+true.W.tilde <- array(true.W.tilde.vec, dim = c(N, q))
 
 # Some known inverse link functions
 
@@ -101,17 +106,23 @@ inv.link <- function(x, link) {
 
 Y.vec <- rep(0, N*q)  
 
-for(i in 1: N*q){
+for(i in 1:N*q){
   if(i %% q == 1){
-    Y.vec[i] = rpois(1, lambda = inv.link(W.vec[i], link = "exp"))
+    Y.vec[i] = true.W.tilde.vec[i]
+      
   }else{
-    Y.vec[i] = rpois(1, lambda = inv.link(W.vec[i], link = "exp"))
+    Y.vec[i] = rpois(1, lambda = inv.link(true.W.tilde.vec[i], link = "exp"))
   }
 }
 
 Y <- array(Y.vec, dim = c(N, q))
+Y.pred.true <- Y[pred.indices,]
+Y.obs <- Y[-pred.indices,]
+
+true.W.tilde.obs <- true.W.tilde[-pred.indices,]
 
 # Saving necessary parameters and data (all in matrix form)
-save(N, p, q, locations, X, Y, W.mat, true.beta,
-     true.Sigma, true.phi, true.nu, true.r, file = "joint.sglmm.simulation.Rdata")
+save(N, N.obs, N.pred, p, q, joint.loc, obs.loc, pred.loc, X.obs, X.pred, 
+     Y.obs, Y.pred.true, true.W.tilde.obs, true.beta, true.Sigma, true.phi, true.nu, true.r, 
+     file = "sglmm.simulation.Rdata")
 
